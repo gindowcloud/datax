1adb63743c7a96d8ce53aeba70a049d949d7b6c4
Joming He <joming.he@qq.com>
2023-04-07 01:53:20 +0800

    容器运行

--- backend/app.py
+++ backend/app.py
@@ -4,4 +4,4 @@ from src import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run('app:app', host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run('app:app', host="0.0.0.0", port=8090, reload=True)
